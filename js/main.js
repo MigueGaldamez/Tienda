e89f90795fed72939432e8745b58f7280f9e6d7b
Mercy Galdámez <Mercy Galdmez>
2017-02-17 16:40:27 -0600

    Primero

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function(){
+
+      $(".button-collapse").sideNav();
+      $('.carousel').carousel();
+    
+      $('.collapsible').collapsible();  
+      $('.parallax').parallax();
+      $('.slider').slider({ });
+      $(".dropdown-button").dropdown({hover:true,belowOrigin:true}) ;
+      $('.carousel.carousel-slider').carousel({fullWidth:true});
+      
+
+      
+});
